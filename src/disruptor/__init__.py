7ed5c0cb16c6b6fa732f44b67d6da69d09d6960c
AlexandrMilko <milkoaleksandr307@gmail.com>
2023-07-30 20:27:22 +0200

    Move SECRET_KEY to environment

--- src/disruptor/__init__.py
+++ src/disruptor/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "8a9060645436c83806084ef6aa20547c"
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", None)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
