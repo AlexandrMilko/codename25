2fe7a5f20dfb135d9b5545b4016bd2961317af48
Oleksandr <milkoaleksandr307@gmail.com>
2024-05-12 15:09:06 +0200

    Refactor code to use calculate rendering params in FurniturePiece. Create window mask for curtains position estimation

--- src/disruptor/stage/Bedroom.py
+++ src/disruptor/stage/Bedroom.py
@@ -1,12 +1,12 @@
 from disruptor.stage.Room import Room
 from disruptor.stage.FurniturePiece import FurniturePiece, Bed, Curtain
+from disruptor.tools import calculate_angle_from_top_view
 import numpy as np
 import os
 from math import radians
 
 
 class Bedroom(Room):
-    furniture_pieces = [Bed(), Curtain()]
 
     def stage(self, current_user_id):
         roll, pitch = np.negative(np.degrees(self.find_roll_pitch(current_user_id)))
@@ -14,15 +14,22 @@ class Bedroom(Room):
         compensate_pitch = -radians(pitch)
         compensate_roll = -radians(roll)
 
-        for i in range(len(walls)):
-            try:
-                furniture_piece = self.furniture_pieces[i]
-                wall = walls[i]
-                render_directory = f'disruptor/static/images/{current_user_id}/preprocessed/furniture_render'
-                wall.save_mask(os.path.join(render_directory, 'wall_mask.png'))
-                pixels_for_placing = furniture_piece.find_placement_pixel(os.path.join(render_directory, 'wall_mask.png'))
-                yaw_angle = wall.find_angle_from_3d(self, compensate_pitch, compensate_roll)
-                for pixel in pixels_for_placing:
-                    self.add_furniture(furniture_piece, pixel, yaw_angle, (roll, pitch), current_user_id)
-            except IndexError:
-                break
+        # Add Bed
+        bed = Bed()
+        wall = walls[0]
+        render_directory = f'disruptor/static/images/{current_user_id}/preprocessed/furniture_render'
+        wall.save_mask(os.path.join(render_directory, 'wall_mask.png'))
+        pixels_for_placing = bed.find_placement_pixel(os.path.join(render_directory, 'wall_mask.png'))
+        yaw_angle = wall.find_angle_from_3d(self, compensate_pitch, compensate_roll)
+        for pixel in pixels_for_placing:
+            bed.calculate_rendering_parameters(self, pixel, yaw_angle, (roll, pitch), current_user_id)
+
+        # Add curtains
+        curtain = Curtain()
+        Room.save_windows_mask(f'disruptor/static/images/{current_user_id}/preprocessed/windows_mask.png',
+                               current_user_id)
+        pixels_for_placing = curtain.find_placement_pixel(
+            f'disruptor/static/images/{current_user_id}/preprocessed/windows_mask.png')
+        yaw_angle = calculate_angle_from_top_view(*[self.infer3d(pixel, compensate_pitch, compensate_roll) for pixel in pixels_for_placing])
+        for pixel in pixels_for_placing:
+            curtain.calculate_rendering_parameters(self, pixel, yaw_angle, (roll, pitch), current_user_id)
