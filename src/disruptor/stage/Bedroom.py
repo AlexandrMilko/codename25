885d7df8cf1813b8b020f1c3a075b2be4f84c6df
Oleksandr <milkoaleksandr307@gmail.com>
2024-05-12 13:02:51 +0200

    Pass params to find_angle_from_3d in Bedroom.py

--- src/disruptor/stage/Bedroom.py
+++ src/disruptor/stage/Bedroom.py
@@ -2,6 +2,7 @@ from disruptor.stage.Room import Room
 from disruptor.stage.FurniturePiece import FurniturePiece, Bed
 import numpy as np
 import os
+from math import radians
 
 
 class Bedroom(Room):
@@ -10,6 +11,8 @@ class Bedroom(Room):
     def stage(self, current_user_id):
         roll, pitch = np.negative(np.degrees(self.find_roll_pitch(current_user_id)))
         walls = self.get_walls(current_user_id)
+        compensate_pitch = -radians(pitch)
+        compensate_roll = -radians(roll)
 
         for i in range(len(walls)):
             try:
@@ -18,7 +21,7 @@ class Bedroom(Room):
                 render_directory = f'disruptor/static/images/{current_user_id}/preprocessed/furniture_render'
                 wall.save_mask(os.path.join(render_directory, 'wall_mask.png'))
                 pixel_for_placing = furniture_piece.find_placement_pixel(os.path.join(render_directory, 'wall_mask.png'))
-                yaw_angle = wall.find_angle_from_3d()
+                yaw_angle = wall.find_angle_from_3d(self, compensate_pitch, compensate_roll)
                 self.add_furniture(furniture_piece, pixel_for_placing, yaw_angle, (roll, pitch), current_user_id)
             except IndexError:
                 break
