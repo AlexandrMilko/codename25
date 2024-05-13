ea044d5604ce7cbd988e61079cd2252912842540
Oleksandr <milkoaleksandr307@gmail.com>
2024-05-13 12:27:53 +0200

    Add Log for curtains pixels

--- src/disruptor/stage/Bedroom.py
+++ src/disruptor/stage/Bedroom.py
@@ -30,6 +30,7 @@ class Bedroom(Room):
                                current_user_id)
         pixels_for_placing = curtain.find_placement_pixel(
             f'disruptor/static/images/{current_user_id}/preprocessed/windows_mask.png')
+        print(pixels_for_placing, "Curtains pixels")
         for window in pixels_for_placing:
             for left_top_point, right_top_point in window:
                 print(left_top_point, right_top_point, "Left and right top points")
