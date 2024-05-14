e2ced181dc9d8b5e2f8a95e325ffc3d6afeddf43
Oleksandr <milkoaleksandr307@gmail.com>
2024-05-14 13:53:17 +0200

    Add floor centroid calculation and estimating camera height based on its z coordinate

--- src/disruptor/stage/FurniturePiece.py
+++ src/disruptor/stage/FurniturePiece.py
@@ -203,6 +203,7 @@ class Curtain(FurniturePiece):
         # We set opposite
         camera_angles = radians(
             90) + compensate_pitch, -compensate_roll, 0  # We add 90 to the pitch, because originally camera is rotated pointing downwards in Blender
+        # TODO Perform camera height estimation not here, but in stage() function to save computing power
         camera_height = room.estimate_camera_height((compensate_pitch, compensate_roll), current_user_id)
         camera_location = 0, 0, camera_height
 
