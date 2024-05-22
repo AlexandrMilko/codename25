58738cd1e611ab63eeb82df39652c3b9d5d6d47f
Oleksandr <milkoaleksandr307@gmail.com>
2024-05-22 16:38:49 +0200

    Negate all pitch, roll angles except ones used to calculate camera orientation for Blender

--- src/disruptor/stage/FurniturePiece.py
+++ src/disruptor/stage/FurniturePiece.py
@@ -92,9 +92,9 @@ class Bed(FurniturePiece):
         obj_scale = self.get_scale()
         # We set opposite
         camera_angles = radians(
-            90) + compensate_pitch, -compensate_roll, 0  # We add 90 to the pitch, because originally camera is rotated pointing downwards in Blender
+            90) - compensate_pitch, +compensate_roll, 0  # We add 90 to the pitch, because originally camera is rotated pointing downwards in Blender
         print("Started estimating camera height")
-        camera_height = room.estimate_camera_height((radians(pitch), radians(roll)), current_user_id)
+        camera_height = room.estimate_camera_height((compensate_pitch, compensate_roll), current_user_id)
         print(f"Camera height: {camera_height}")
         camera_location = 0, 0, camera_height
         obj_offsets_floor = obj_offsets.copy()
@@ -232,9 +232,9 @@ class Curtain(FurniturePiece):
         obj_scale = self.get_scale()
         # We set opposite
         camera_angles = radians(
-            90) + compensate_pitch, -compensate_roll, 0  # We add 90 to the pitch, because originally camera is rotated pointing downwards in Blender
+            90) - compensate_pitch, compensate_roll, 0  # We add 90 to the pitch, because originally camera is rotated pointing downwards in Blender
         # TODO Perform camera height estimation not here, but in stage() function to save computing power
-        camera_height = room.estimate_camera_height((radians(pitch), radians(roll)), current_user_id)
+        camera_height = room.estimate_camera_height((compensate_pitch, compensate_roll), current_user_id)
         camera_location = 0, 0, camera_height
 
         print("Curtain coords")
