91754e52b88bd224f6b27ee8dd8b3a744566ece9
Oleksandr <milkoaleksandr307@gmail.com>
2024-05-24 12:55:37 +0200

    Add logs for biggest wall search

--- src/disruptor/stage/Wall.py
+++ src/disruptor/stage/Wall.py
@@ -145,6 +145,21 @@ class Wall:
         markers, _ = ndimage.label(mask)
         labels = watershed(-distance, markers, mask=image)
 
+        fig, axes = plt.subplots(ncols=3, figsize=(9, 3), sharex=True, sharey=True)
+        ax = axes.ravel()
+
+        ax[0].imshow(image, cmap=plt.cm.gray)
+        ax[0].set_title('Overlapping objects')
+        ax[1].imshow(-distance, cmap=plt.cm.gray)
+        ax[1].set_title('Distances')
+        ax[2].imshow(labels, cmap=plt.cm.nipy_spectral)
+        ax[2].set_title('Separated objects')
+
+        for a in ax:
+            a.set_axis_off()
+        fig.tight_layout()
+        plt.show()
+
         biggest_wall_class = Wall.get_biggest_wall_class(labels)
         print(biggest_wall_class, "biggest_wall_class")
 
