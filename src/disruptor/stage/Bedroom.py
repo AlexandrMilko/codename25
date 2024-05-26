dfad561d131a1bc3561b2a6a6ac01246d33113bf
Oleksandr <milkoaleksandr307@gmail.com>
2024-05-26 17:40:01 +0200

    Add exception for cases when curtains point is out of image boundaries

--- src/disruptor/stage/Bedroom.py
+++ src/disruptor/stage/Bedroom.py
@@ -46,19 +46,22 @@ class Bedroom(Room):
         print(f"CURTAINS placement pixels: {pixels_for_placing}")
         Image.open(self.original_image_path).save(prerequisite_path)
         for window in pixels_for_placing:
-            left_top_point, right_top_point = window
-            yaw_angle = calculate_angle_from_top_view(*[self.infer_3d(pixel, pitch_rad, roll_rad) for
-                                                        pixel in (left_top_point, right_top_point)])
-            for pixel in (left_top_point, right_top_point):
-                render_parameters = curtain.calculate_rendering_parameters(self, pixel, yaw_angle,
-                                                                           (roll_rad, pitch_rad), current_user_id)
-                width, height = get_image_size(self.original_image_path)
-                render_parameters['resolution_x'] = width
-                render_parameters['resolution_y'] = height
-                curtain_image = curtain.request_blender_render(render_parameters)
-                background_image = Image.open(prerequisite_path)
-                combined_image = image_overlay(curtain_image, background_image)
-                combined_image.save(prerequisite_path)
+            try:
+                left_top_point, right_top_point = window
+                yaw_angle = calculate_angle_from_top_view(*[self.infer_3d(pixel, pitch_rad, roll_rad) for
+                                                            pixel in (left_top_point, right_top_point)])
+                for pixel in (left_top_point, right_top_point):
+                    render_parameters = curtain.calculate_rendering_parameters(self, pixel, yaw_angle,
+                                                                               (roll_rad, pitch_rad), current_user_id)
+                    width, height = get_image_size(self.original_image_path)
+                    render_parameters['resolution_x'] = width
+                    render_parameters['resolution_y'] = height
+                    curtain_image = curtain.request_blender_render(render_parameters)
+                    background_image = Image.open(prerequisite_path)
+                    combined_image = image_overlay(curtain_image, background_image)
+                    combined_image.save(prerequisite_path)
+            except IndexError as e:
+                print(f"{e}, we skip adding curtains for a window.")
 
         # Add time for Garbage Collector
         time.sleep(5)
