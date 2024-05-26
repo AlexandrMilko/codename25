31b5e55992428b36ded335383040407d8620eb8a
Oleksandr <milkoaleksandr307@gmail.com>
2024-05-26 17:09:23 +0200

    Add plant

--- src/disruptor/stage/Bedroom.py
+++ src/disruptor/stage/Bedroom.py
@@ -1,5 +1,6 @@
 from disruptor.stage.Room import Room
-from disruptor.stage.FurniturePiece import FurniturePiece, Bed, Curtain
+from disruptor.stage.Floor import Floor
+from disruptor.stage.FurniturePiece import FurniturePiece, Bed, Curtain, Plant
 from disruptor.tools import calculate_angle_from_top_view, get_image_size
 import numpy as np
 import os
@@ -62,20 +63,42 @@ class Bedroom(Room):
         # Add time for Garbage Collector
         time.sleep(5)
 
+        # Add plant
+        plant = Plant()
+        seg_image_path = f'disruptor/static/images/{current_user_id}/preprocessed/segmented_es.png'
+        save_path = 'disruptor/static/images/floor_mask.png'
+        Floor.save_mask(seg_image_path, save_path)
+        pixels_for_placing = plant.find_placement_pixel(save_path)
+        print(f"PLANT placement pixels: {pixels_for_placing}")
+        import random
+        random_index = random.randint(0, len(pixels_for_placing) - 1)
+        render_parameters = (
+            plant.calculate_rendering_parameters(self, pixels_for_placing[random_index], (roll_rad, pitch_rad),
+                                               current_user_id))
+        width, height = get_image_size(self.original_image_path)
+        render_parameters['resolution_x'] = width
+        render_parameters['resolution_y'] = height
+        plant_image = plant.request_blender_render(render_parameters)
+        background_image = Image.open(prerequisite_path)
+        combined_image = image_overlay(plant_image, background_image)
+        combined_image.save(prerequisite_path)
+
+        # Add time for Garbage Collector
+        time.sleep(5)
+
         # Add Bed
         bed = Bed()
         wall = self.get_biggest_wall(current_user_id)
         render_directory = f'disruptor/static/images/{current_user_id}/preprocessed/furniture_render'
         wall.save_mask(os.path.join(render_directory, 'wall_mask.png'))
-        pixels_for_placing = bed.find_placement_pixel(os.path.join(render_directory, 'wall_mask.png'))
-        print(f"BED placement pixel: {pixels_for_placing}")
+        pixel_for_placing = bed.find_placement_pixel(os.path.join(render_directory, 'wall_mask.png'))
+        print(f"BED placement pixel: {pixel_for_placing}")
         yaw_angle = wall.find_angle_from_3d(self, pitch_rad, roll_rad)
-        for pixel in pixels_for_placing:
-            render_parameters = (bed.calculate_rendering_parameters(self, pixel, yaw_angle, (roll_rad, pitch_rad), current_user_id))
-            width, height = get_image_size(self.original_image_path)
-            render_parameters['resolution_x'] = width
-            render_parameters['resolution_y'] = height
-            bed_image = bed.request_blender_render(render_parameters)
-            background_image = Image.open(prerequisite_path)
-            combined_image = image_overlay(bed_image, background_image)
-            combined_image.save(prerequisite_path)
+        render_parameters = (bed.calculate_rendering_parameters(self, pixel_for_placing, yaw_angle, (roll_rad, pitch_rad), current_user_id))
+        width, height = get_image_size(self.original_image_path)
+        render_parameters['resolution_x'] = width
+        render_parameters['resolution_y'] = height
+        bed_image = bed.request_blender_render(render_parameters)
+        background_image = Image.open(prerequisite_path)
+        combined_image = image_overlay(bed_image, background_image)
+        combined_image.save(prerequisite_path)
