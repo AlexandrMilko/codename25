1eebe5ff2266c16bd3c447b10a84cb1b29a0a06b
Oleksandr <milkoaleksandr307@gmail.com>
2024-05-28 15:07:39 +0200

    Add CORS support

--- src/disruptor/__init__.py
+++ src/disruptor/__init__.py
@@ -3,8 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", None)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app)
