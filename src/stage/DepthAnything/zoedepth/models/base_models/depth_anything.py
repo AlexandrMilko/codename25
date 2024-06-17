1eb53c6db3ee6c7813e6d10c5d1400bbb5310b6b
Oleksandr <milkoaleksandr307@gmail.com>
2024-06-17 15:18:32 +0200

    Use depth anything v1

--- src/stage/DepthAnything/zoedepth/models/base_models/depth_anything.py
+++ src/stage/DepthAnything/zoedepth/models/base_models/depth_anything.py
@@ -341,8 +341,7 @@ class DepthAnythingCore(nn.Module):
         
         depth_anything = DPT_DINOv2(out_channels=[256, 512, 1024, 1024], use_clstoken=False)
 
-        state_dict = torch.load('stage/DepthAnything/zoedepth/checkpoints/depth_anything_v2_metric_hypersim_vitl.pth', map_location='cpu')
-        print(state_dict, "state_dict")
+        state_dict = torch.load('stage/DepthAnything/zoedepth/checkpoints/depth_anything_metric_depth_indoor.pt', map_location='cpu')
 
         corrected_state_dict = {key.replace('core.core.', ''): value for key, value in state_dict['model'].items()}
         keys_to_remove = [
