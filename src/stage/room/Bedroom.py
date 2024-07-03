d9adf69790df355474d770ab610b3cfa9c88bccf
arudnytskyi <rudnytskyi.a.v@gmail.com>
2024-07-03 12:58:04 +0200

    Implement Kyrylo's algorithm for kitchen table

--- src/stage/room/Bedroom.py
+++ src/stage/room/Bedroom.py
@@ -7,6 +7,7 @@ import time
 
 from PIL import Image
 
+
 class Bedroom(Room):
 
     def stage(self):
@@ -67,3 +68,27 @@ class Bedroom(Room):
         segmented_es_path = f'images/preprocessed/seg_prerequisite.png'
         Room.save_windows_mask(segmented_es_path,
                                f'images/preprocessed/windows_mask_inpainting.png')
+
+    def add_bed(self, camera_angles_rad: tuple, mask_path, tmp_mask_path, prerequisite_path):
+        from stage.furniture.Bed import Bed
+        from tools import convert_png_to_mask, image_overlay, overlay_masks
+        pitch_rad, roll_rad = camera_angles_rad
+        bed = Bed()
+        wall = self.get_biggest_wall()
+        render_directory = f'images/preprocessed'
+        wall.save_mask(os.path.join(render_directory, 'wall_mask.png'))
+        pixel_for_placing = bed.find_placement_pixel(os.path.join(render_directory, 'wall_mask.png'))
+        print(f"BED placement pixel: {pixel_for_placing}")
+        yaw_angle = wall.find_angle_from_3d(self, pitch_rad, roll_rad)
+        render_parameters = (
+            bed.calculate_rendering_parameters(self, pixel_for_placing, yaw_angle, (roll_rad, pitch_rad)))
+        width, height = get_image_size(self.empty_room_image_path)
+        render_parameters['resolution_x'] = width
+        render_parameters['resolution_y'] = height
+        bed_image = bed.request_blender_render(render_parameters)
+        bed_image.save(tmp_mask_path)
+        convert_png_to_mask(tmp_mask_path)
+        overlay_masks(tmp_mask_path, mask_path, mask_path, [0, 0])
+        background_image = Image.open(prerequisite_path)
+        combined_image = image_overlay(bed_image, background_image)
+        combined_image.save(prerequisite_path)
--- src/stage/room/Kitchen.py
+++ src/stage/room/Kitchen.py
@@ -1,12 +1,11 @@
+from tools import (calculate_angle_from_top_view, get_image_size, create_mask_of_size,
+                   convert_png_to_mask, overlay_masks, run_preprocessor)
 from stage.room.Room import Room
-from tools import calculate_angle_from_top_view, get_image_size, create_mask_of_size, convert_png_to_mask, overlay_masks
-import numpy as np
-import os
 from math import radians
-import time
-
 from PIL import Image
-from tools import image_overlay
+import numpy as np
+import time
+import os
 
 
 class Kitchen(Room):
@@ -56,7 +55,8 @@ class Kitchen(Room):
         time.sleep(5)
 
         # Add plant
-        self.add_plant((pitch_rad, roll_rad), mask_path, tmp_mask_path, prerequisite_path)
+        # TODO change algo for plant with new Kyrylo algorithm
+        # self.add_plant((pitch_rad, roll_rad), mask_path, tmp_mask_path, prerequisite_path)
 
         # Add time for Garbage Collector
         time.sleep(5)
@@ -64,6 +64,56 @@ class Kitchen(Room):
         # Add kitchen_table_with_chairs
         self.add_kitchen_table_with_chairs((pitch_rad, roll_rad), mask_path, tmp_mask_path, prerequisite_path)
 
+        run_preprocessor("seg_ofade20k", prerequisite_path, "seg_prerequisite.png", height)
+        segmented_es_path = f'images/preprocessed/seg_prerequisite.png'
+        Room.save_windows_mask(segmented_es_path,
+                               f'images/preprocessed/windows_mask_inpainting.png')
+
+    def add_kitchen_table_with_chairs(self, camera_angles_rad: tuple, mask_path, tmp_mask_path, prerequisite_path):
+        from stage.furniture.KitchenTableWithChairs import KitchenTableWithChairs
+        from stage.Floor import Floor
+        from tools import convert_png_to_mask, image_overlay, overlay_masks
+        import random
+        pitch_rad, roll_rad = camera_angles_rad
+
+        kitchen_table_with_chairs = KitchenTableWithChairs()
+        seg_image_path = f'images/preprocessed/segmented_es.png'
+        save_path = 'images/preprocessed/floor_mask.png'
+        Floor.save_mask(seg_image_path, save_path)
+
+        pixels_for_placing = kitchen_table_with_chairs.find_pixel_placement('images/preprocessed/floor_layout.png')
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        print(pixels_for_placing)
+
+        # pixels_for_placing = kitchen_table_with_chairs.find_placement_pixel(save_path)
+        print(f"KitchenTableWithChairs placement pixel: {pixels_for_placing}")
+        wall = self.get_biggest_wall()
+        render_directory = f'images/preprocessed/'
+        wall.save_mask(os.path.join(render_directory, 'wall_mask.png'))
+        yaw_angle = wall.find_angle_from_3d(self, pitch_rad, roll_rad)
+        random_index = random.randint(0, len(pixels_for_placing) - 1)
+        render_parameters = (
+            kitchen_table_with_chairs.calculate_rendering_parameters(self, pixels_for_placing[0], yaw_angle,
+                                                                     (roll_rad, pitch_rad)))
+        width, height = get_image_size(self.empty_room_image_path)
+        render_parameters['resolution_x'] = width
+        render_parameters['resolution_y'] = height
+        table_image = kitchen_table_with_chairs.request_blender_render(render_parameters)
+        table_image.save(tmp_mask_path)
+        convert_png_to_mask(tmp_mask_path)
+        overlay_masks(tmp_mask_path, mask_path, mask_path, [0, 0])
+        background_image = Image.open(prerequisite_path)
+        combined_image = image_overlay(table_image, background_image)
+        combined_image.save(prerequisite_path)
+
+        # Create windows mask for staged room
         run_preprocessor("seg_ofade20k", prerequisite_path, "seg_prerequisite.png", height)
         segmented_es_path = f'images/preprocessed/seg_prerequisite.png'
         Room.save_windows_mask(segmented_es_path,
