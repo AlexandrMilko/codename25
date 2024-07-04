8ae2f33e341ad6118f17e8dcca4057ceb644c652
Oleksandr <milkoaleksandr307@gmail.com>
2024-07-04 16:05:18 +0200

    Check timings for adding furniture

--- src/stage/room/Room.py
+++ src/stage/room/Room.py
@@ -175,14 +175,8 @@ class Room:
                 yaw_angle = calculate_angle_from_top_view(*[self.infer_3d(pixel, pitch_rad, roll_rad) for
                                                             pixel in (left_top_point, right_top_point)])
                 for pixel in (left_top_point, right_top_point):
-                    import time
-                    start_time = time.time()
                     render_parameters = curtain.calculate_rendering_parameters(self, pixel, yaw_angle,
                                                                                (roll_rad, pitch_rad))
-                    end_time = time.time()
-                    execution_time = end_time - start_time
-                    print(f"Calculating rendering params Execution time: {execution_time} seconds")
-                    
                     width, height = get_image_size(self.empty_room_image_path)
                     render_parameters['resolution_x'] = width
                     render_parameters['resolution_y'] = height
@@ -190,6 +184,8 @@ class Room:
                     curtains_height_scale = curtains_height / Curtain.default_height
                     render_parameters['obj_scale'] = render_parameters['obj_scale'][0], render_parameters['obj_scale'][
                         1], curtains_height_scale
+                    import time
+                    start_time = time.time()
                     curtain_image = curtain.request_blender_render(render_parameters)
                     curtain_image.save(tmp_mask_path)
                     convert_png_to_mask(tmp_mask_path)
@@ -197,6 +193,9 @@ class Room:
                     background_image = Image.open(prerequisite_path)
                     combined_image = image_overlay(curtain_image, background_image)
                     combined_image.save(prerequisite_path)
+                    end_time = time.time()
+                    execution_time = end_time - start_time
+                    print(f"Render and process image Execution time: {execution_time} seconds")
 
             except IndexError as e:
                 print(f"{e}, we skip adding curtains for a window.")
