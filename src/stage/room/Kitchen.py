48924740ccebd702fbb69ba8b3f1e1fcc89edad8
arudnytskyi <rudnytskyi.a.v@gmail.com>
2024-07-05 15:23:01 +0200

    resolve merge conflict 2

--- src/stage/room/Kitchen.py
+++ src/stage/room/Kitchen.py
@@ -69,43 +69,43 @@ class Kitchen(Room):
         Room.save_windows_mask(segmented_es_path,
                                f'images/preprocessed/windows_mask_inpainting.png')
 
-    def add_kitchen_table_with_chairs(self, camera_angles_rad: tuple, mask_path, tmp_mask_path, prerequisite_path):
-        from stage.furniture.KitchenTableWithChairs import KitchenTableWithChairs
-        from stage.Floor import Floor
-        from tools import convert_png_to_mask, image_overlay, overlay_masks
-        import random
-        pitch_rad, roll_rad = camera_angles_rad
-
-        kitchen_table_with_chairs = KitchenTableWithChairs()
-        seg_image_path = f'images/preprocessed/segmented_es.png'
-        save_path = 'images/preprocessed/floor_mask.png'
-        Floor.save_mask(seg_image_path, save_path)
-
-        kitchen_table_with_chairs.find_placement_pixel_from_floor_layout('images/preprocessed/floor_layout.png')
-
-        pixels_for_placing = kitchen_table_with_chairs.find_placement_pixel(save_path)
-        print(f"KitchenTableWithChairs placement pixel: {pixels_for_placing}")
-        wall = self.get_biggest_wall()
-        render_directory = f'images/preprocessed/'
-        wall.save_mask(os.path.join(render_directory, 'wall_mask.png'))
-        yaw_angle = wall.find_angle_from_3d(self, pitch_rad, roll_rad)
-        random_index = random.randint(0, len(pixels_for_placing) - 1)
-        render_parameters = (
-            kitchen_table_with_chairs.calculate_rendering_parameters(self, pixels_for_placing[random_index], yaw_angle,
-                                                                     (roll_rad, pitch_rad)))
-        width, height = get_image_size(self.empty_room_image_path)
-        render_parameters['resolution_x'] = width
-        render_parameters['resolution_y'] = height
-        table_image = kitchen_table_with_chairs.request_blender_render(render_parameters)
-        table_image.save(tmp_mask_path)
-        convert_png_to_mask(tmp_mask_path)
-        overlay_masks(tmp_mask_path, mask_path, mask_path)
-        background_image = Image.open(prerequisite_path)
-        combined_image = image_overlay(table_image, background_image)
-        combined_image.save(prerequisite_path)
-
-        # Create windows mask for staged room
-        run_preprocessor("seg_ofade20k", prerequisite_path, "seg_prerequisite.png", height)
-        segmented_es_path = f'images/preprocessed/seg_prerequisite.png'
-        Room.save_windows_mask(segmented_es_path,
-                               f'images/preprocessed/windows_mask_inpainting.png')
+    # def add_kitchen_table_with_chairs(self, camera_angles_rad: tuple, mask_path, tmp_mask_path, prerequisite_path):
+    #     from stage.furniture.KitchenTableWithChairs import KitchenTableWithChairs
+    #     from stage.Floor import Floor
+    #     from tools import convert_png_to_mask, image_overlay, overlay_masks
+    #     import random
+    #     pitch_rad, roll_rad = camera_angles_rad
+    #
+    #     kitchen_table_with_chairs = KitchenTableWithChairs()
+    #     seg_image_path = f'images/preprocessed/segmented_es.png'
+    #     save_path = 'images/preprocessed/floor_mask.png'
+    #     Floor.save_mask(seg_image_path, save_path)
+    #
+    #     kitchen_table_with_chairs.find_placement_pixel_from_floor_layout('images/preprocessed/floor_layout.png')
+    #
+    #     pixels_for_placing = kitchen_table_with_chairs.find_placement_pixel(save_path)
+    #     print(f"KitchenTableWithChairs placement pixel: {pixels_for_placing}")
+    #     wall = self.get_biggest_wall()
+    #     render_directory = f'images/preprocessed/'
+    #     wall.save_mask(os.path.join(render_directory, 'wall_mask.png'))
+    #     yaw_angle = wall.find_angle_from_3d(self, pitch_rad, roll_rad)
+    #     random_index = random.randint(0, len(pixels_for_placing) - 1)
+    #     render_parameters = (
+    #         kitchen_table_with_chairs.calculate_rendering_parameters(self, pixels_for_placing[random_index], yaw_angle,
+    #                                                                  (roll_rad, pitch_rad)))
+    #     width, height = get_image_size(self.empty_room_image_path)
+    #     render_parameters['resolution_x'] = width
+    #     render_parameters['resolution_y'] = height
+    #     table_image = kitchen_table_with_chairs.request_blender_render(render_parameters)
+    #     table_image.save(tmp_mask_path)
+    #     convert_png_to_mask(tmp_mask_path)
+    #     overlay_masks(tmp_mask_path, mask_path, mask_path)
+    #     background_image = Image.open(prerequisite_path)
+    #     combined_image = image_overlay(table_image, background_image)
+    #     combined_image.save(prerequisite_path)
+    #
+    #     # Create windows mask for staged room
+    #     run_preprocessor("seg_ofade20k", prerequisite_path, "seg_prerequisite.png", height)
+    #     segmented_es_path = f'images/preprocessed/seg_prerequisite.png'
+    #     Room.save_windows_mask(segmented_es_path,
+    #                            f'images/preprocessed/windows_mask_inpainting.png')
