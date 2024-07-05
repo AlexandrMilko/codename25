d9f9e18cbc1066a76a95e346199179fef7b18aeb
Oleksandr <milkoaleksandr307@gmail.com>
2024-07-05 12:09:42 +0200

    Remove timings

--- src/stage/room/Room.py
+++ src/stage/room/Room.py
@@ -186,27 +186,10 @@ class Room:
                         1], curtains_height_scale
                     curtain_image = curtain.request_blender_render(render_parameters)
                     curtain_image.save(tmp_mask_path)
-
-                    import time
-                    start_time = time.time()
                     convert_png_to_mask(tmp_mask_path)
-                    end_time = time.time()
-                    execution_time = end_time - start_time
-                    print(f"convert_png_to_mask Execution time: {execution_time} seconds")
-
-                    start_time = time.time()
                     overlay_masks(tmp_mask_path, mask_path, mask_path)
-                    end_time = time.time()
-                    execution_time = end_time - start_time
-                    print(f"overlay_masks Execution time: {execution_time} seconds")
-
                     background_image = Image.open(prerequisite_path)
-
-                    start_time = time.time()
                     combined_image = image_overlay(curtain_image, background_image)
-                    end_time = time.time()
-                    execution_time = end_time - start_time
-                    print(f"image_overlay Execution time: {execution_time} seconds")
 
                     combined_image.save(prerequisite_path)
 
