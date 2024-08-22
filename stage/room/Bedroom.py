0d48d17326dfb8a31f4f4cae190f864b59b3c229
Oleksandr <milkoaleksandr307@gmail.com>
2024-08-22 13:24:11 +0200

    Render with new Floor Layout calculation

--- stage/room/Bedroom.py
+++ stage/room/Bedroom.py
@@ -18,7 +18,8 @@ class Bedroom(Room):
         bed_parameters = self.calculate_bed_parameters((pitch_rad, roll_rad))
 
         scene_render_parameters['objects'] = [bed_parameters]
-        print(scene_render_parameters)
+        import json
+        print(json.dumps(scene_render_parameters, indent = 4))
 
         # Add curtains
         # curtains_parameters = self.calculate_curtains_parameters(camera_height, (pitch_rad, roll_rad))
@@ -32,22 +33,19 @@ class Bedroom(Room):
         #
         # scene_render_parameters['objects'] = [*curtains_parameters, bed_parameters]
         #
-        # import json
-        # print(json.dumps(scene_render_parameters, indent = 4))
-        #
-        # furniture_image = Furniture.request_blender_render(scene_render_parameters)
-        # Room.process_rendered_image(furniture_image)
-        #
-        # processor = PostProcessor()
-        # processor.execute()
-        #
-        # # Create windows mask for staged room
-        # PREPROCESSOR_RESOLUTION_LIMIT = 1024 if height > 1024 else height
-        # segment = ImageSegmentor(Path.PREREQUISITE_IMAGE.value, Path.SEG_PREREQUISITE_IMAGE.value, PREPROCESSOR_RESOLUTION_LIMIT)
-        # segment.execute()
-        # resize_and_save_image(Path.SEG_PREREQUISITE_IMAGE.value,
-        #                       Path.SEG_PREREQUISITE_IMAGE.value, height)
-        # Room.save_windows_mask(Path.SEG_PREREQUISITE_IMAGE.value, Path.WINDOWS_MASK_INPAINTING_IMAGE.value)
+        furniture_image = Furniture.request_blender_render(scene_render_parameters)
+        Room.process_rendered_image(furniture_image)
+
+        processor = PostProcessor()
+        processor.execute()
+
+        # Create windows mask for staged room
+        PREPROCESSOR_RESOLUTION_LIMIT = 1024 if height > 1024 else height
+        segment = ImageSegmentor(Path.PREREQUISITE_IMAGE.value, Path.SEG_PREREQUISITE_IMAGE.value, PREPROCESSOR_RESOLUTION_LIMIT)
+        segment.execute()
+        resize_and_save_image(Path.SEG_PREREQUISITE_IMAGE.value,
+                              Path.SEG_PREREQUISITE_IMAGE.value, height)
+        Room.save_windows_mask(Path.SEG_PREREQUISITE_IMAGE.value, Path.WINDOWS_MASK_INPAINTING_IMAGE.value)
 
 
     def calculate_bed_parameters(self, camera_angles_rad: tuple):
