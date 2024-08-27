d8ced109e23af143522a32a04c358e328acd01b8
Oleksandr <milkoaleksandr307@gmail.com>
2024-08-27 19:45:20 +0200

    Add commode

---
+++ stage/furniture/Commode.py
@@ -0,0 +1,6 @@
+from .Furniture import FloorFurniture
+from constants import Path
+
+class Commode(FloorFurniture):
+    def __init__(self, model_path=Path.COMMODE_MODEL.value):
+        super().__init__(model_path)
