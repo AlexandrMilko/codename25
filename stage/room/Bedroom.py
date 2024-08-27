cdf66cf5b639010efd918fd14c5e618d23f383cd
Oleksandr <milkoaleksandr307@gmail.com>
2024-08-27 19:12:53 +0200

    Calculate all layout sides before staging

--- stage/room/Bedroom.py
+++ stage/room/Bedroom.py
@@ -17,7 +17,10 @@ class Bedroom(Room):
         area = self.floor_layout.estimate_area_from_floor_layout()
         print(area, "AREA in m2")
 
-        bed_parameters = self.calculate_bed_parameters((pitch_rad, roll_rad))
+        all_sides = self.floor_layout.find_all_sides_sorted_by_length()
+        print(all_sides, "ALL SIDES")
+
+        bed_parameters = self.calculate_bed_parameters(all_sides.pop(0), (pitch_rad, roll_rad))
         plant_parameters = self.calculate_plant_parameters((pitch_rad, roll_rad))
         curtains_parameters = self.calculate_curtains_parameters(camera_height, (pitch_rad, roll_rad))
 
@@ -37,32 +40,27 @@ class Bedroom(Room):
         furniture_image = Furniture.request_blender_render(scene_render_parameters)
         Room.process_rendered_image(furniture_image)
 
-        processor = PostProcessor()
-        processor.execute()
+        # processor = PostProcessor()
+        # processor.execute()
+        #
+        # # Create windows mask for staged room
+        # PREPROCESSOR_RESOLUTION_LIMIT = 1024 if height > 1024 else height
+        # segment = ImageSegmentor(Path.PREREQUISITE_IMAGE.value, Path.SEG_PREREQUISITE_IMAGE.value, PREPROCESSOR_RESOLUTION_LIMIT)
+        # segment.execute()
+        # resize_and_save_image(Path.SEG_PREREQUISITE_IMAGE.value,
+        #                       Path.SEG_PREREQUISITE_IMAGE.value, height)
+        # Room.save_windows_mask(Path.SEG_PREREQUISITE_IMAGE.value, Path.WINDOWS_MASK_INPAINTING_IMAGE.value)
 
-        # Create windows mask for staged room
-        PREPROCESSOR_RESOLUTION_LIMIT = 1024 if height > 1024 else height
-        segment = ImageSegmentor(Path.PREREQUISITE_IMAGE.value, Path.SEG_PREREQUISITE_IMAGE.value, PREPROCESSOR_RESOLUTION_LIMIT)
-        segment.execute()
-        resize_and_save_image(Path.SEG_PREREQUISITE_IMAGE.value,
-                              Path.SEG_PREREQUISITE_IMAGE.value, height)
-        Room.save_windows_mask(Path.SEG_PREREQUISITE_IMAGE.value, Path.WINDOWS_MASK_INPAINTING_IMAGE.value)
-
-    def calculate_bed_parameters(self, camera_angles_rad: tuple):
+    def calculate_bed_parameters(self, side, camera_angles_rad: tuple):
         from stage.furniture.Bed import Bed
 
         ratio_x, ratio_y = self.floor_layout.get_pixels_per_meter_ratio()
         pixels_dict = self.floor_layout.get_pixels_dict()
-        # TODO move find_middle_of_longest_side to BED class?
-        longest_side = self.floor_layout.find_middle_of_longest_side()
 
-        all_sides = self.floor_layout.find_all_sides_sorted_by_length()
-        print(all_sides, "ALL SIDES")
-
-        print(longest_side, pixels_dict)
+        print(side, pixels_dict)
         print(ratio_x, ratio_y, "ratios")
 
-        middle_point = longest_side.get_middle_point()
+        middle_point = side.get_middle_point()
         pixel_diff = -1 * (middle_point[0] - pixels_dict['camera'][0][0]), middle_point[1] - pixels_dict['camera'][0][1]
         bed_offset_x_y = self.floor_layout.calculate_offset_from_pixel_diff(pixel_diff, (ratio_x, ratio_y))
         print(bed_offset_x_y, "Bed offset")
@@ -70,7 +68,7 @@ class Bedroom(Room):
         pitch_rad, roll_rad = camera_angles_rad
         bed = Bed()
         print(f"BED floor placement pixel: {middle_point}")
-        yaw_angle = longest_side.calculate_wall_angle()
+        yaw_angle = side.calculate_wall_angle()
         print(yaw_angle, "BED yaw angle in degrees")
         render_parameters = (
             bed.calculate_rendering_parameters(self, bed_offset_x_y, yaw_angle, (roll_rad, pitch_rad)))
