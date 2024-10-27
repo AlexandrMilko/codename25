412c196e4e99e5634502b70ee6636767df410c29
Oleksandr <milkoaleksandr307@gmail.com>
2024-10-27 11:52:02 +0100

    Implement Misha's living room algorithm. Refactor it a little.

--- stage/furniture/__init__.py
+++ stage/furniture/__init__.py
@@ -2,4 +2,4 @@ from .Bed import Bed
 from .Curtain import Curtain
 from .Plant import Plant
 from .KitchenTableWithChairs import KitchenTableWithChairs
-from .SofaWithTable import SofaWithTable
+from .LivingRoomSet import LivingRoomSet
