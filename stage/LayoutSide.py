5d07e7bafae0385685107532b470b3c4ae89613f
arudnytskyi <rudnytskyi.a.v@gmail.com>
2024-12-17 21:13:25 +0100

    place the furniture as far from each other as possible

--- stage/LayoutSide.py
+++ stage/LayoutSide.py
@@ -4,15 +4,16 @@ import math
 
 class LayoutSide:
     def __init__(self, points):
+        self.chosen_model_path = None
         self.points = [list(map(int, point)) for point in points]
         first_point = points[0]
         second_point = points[1]
         self.middle_point = (first_point[0] + second_point[0]) // 2, (first_point[1] + second_point[1]) // 2
 
     def calculate_wall_angle(self, ratio_x, ratio_y):
-        proportioned_middle_point = LayoutSide.preserved_proportions(self.middle_point, ratio_x, ratio_y)
-        proportioned_first_point = LayoutSide.preserved_proportions(self.points[0], ratio_x, ratio_y)
-        proportioned_second_point = LayoutSide.preserved_proportions(self.points[1], ratio_x, ratio_y)
+        proportioned_middle_point = self.preserved_proportions(self.middle_point, ratio_x, ratio_y)
+        proportioned_first_point = self.preserved_proportions(self.points[0], ratio_x, ratio_y)
+        proportioned_second_point = self.preserved_proportions(self.points[1], ratio_x, ratio_y)
         top_point = (proportioned_middle_point[0], 0)
 
         vector_top_to_middle = np.array(proportioned_middle_point) - np.array(top_point)
@@ -43,7 +44,7 @@ class LayoutSide:
         return self.middle_point
 
     def __repr__(self):
-        return f"Floor Layout Side: {self.points}, {self.middle_point}"
+        return f'Layout Side: {self.points}, {self.middle_point}'
 
     @staticmethod
     def preserved_proportions(point, ratio_x, ratio_y):
--- stage/furniture/Wardrobe.py
+++ stage/furniture/Wardrobe.py
@@ -1,6 +1,7 @@
 from .Furniture import FloorFurniture
 from constants import Path
 
+
 class Wardrobe(FloorFurniture):
     def __init__(self, model_path=Path.WARDROBE_MODEL.value):
         super().__init__(model_path)
